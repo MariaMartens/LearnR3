805bbce962ae2f9df128ed6f6a2f69b92f5bc565
Maria <mamala@regionsjaelland.dk>
2023-04-25 15:43:52 +0200

    commiting folder

---
+++ R/learning.R
@@ -0,0 +1,15 @@
+# Basics of R -------------------------------------------------------------
+
+colnames(airquality)
+
+str(airquality) # str=structure, shows the structure of the object
+
+summary(airquality)
+
+1 + 1
+
+mean(2:6)
+
+# Loading packages --------------------------------------------------------
+
+library(tidyverse)
